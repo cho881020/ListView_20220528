3a09da43e01ae90e386e379b42d986e9f7368b34
조경진 <cho881020@gmail.com>
2022-05-28 12:17:12 +0900

    리스트뷰에 뿌려줄, 실제 데이터들을 목록에 담아두기

--- app/src/main/java/com/neppplus/listview_20220528/MainActivity.kt
+++ app/src/main/java/com/neppplus/listview_20220528/MainActivity.kt
@@ -2,10 +2,23 @@ package com.neppplus.listview_20220528
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.listview_20220528.datas.StudentData
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<StudentData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add(  StudentData( "조경진", 1988, "서울시 동대문구" )  )
+        mStudentList.add(  StudentData( "강도훈", 1997, "서울시 구로구" )  )
+        mStudentList.add(  StudentData( "권석현", 1982, "서울시 동대문구" )  )
+        mStudentList.add(  StudentData( "박대윤", 1988, "서울시 은평구" )  )
+        mStudentList.add(  StudentData( "이무한", 1977, "서울시 성북구" )  )
+        mStudentList.add(  StudentData( "이석", 1979, "서울시 동작구" )  )
+        mStudentList.add(  StudentData( "이상민", 1979, "서울시 동대문구" )  )
+
     }
 }
