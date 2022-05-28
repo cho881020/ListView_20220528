092a1562645be5df4c29f1b6e5ecf29e68013113
조경진 <cho881020@gmail.com>
2022-05-28 11:38:57 +0900

    학생 데이터를 xml과 결합해서 리스트뷰에 뿌려주는, Adapter 클래스 제작 중

---
+++ app/src/main/java/com/neppplus/listview_20220528/adapters/StudentAdapter.kt
@@ -0,0 +1,7 @@
+package com.neppplus.listview_20220528.adapters
+
+import android.widget.ArrayAdapter
+import com.neppplus.listview_20220528.datas.StudentData
+
+class StudentAdapter : ArrayAdapter<StudentData>() {
+}
