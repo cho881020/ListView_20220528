f49451cb79a271ca96526125aca62ced2f5cd01f
조경진 <cho881020@gmail.com>
2022-05-28 12:08:24 +0900

    어댑터 뼈대작업 마무리

--- app/src/main/java/com/neppplus/listview_20220528/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20220528/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.listview_20220528.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.listview_20220528.R
 import com.neppplus.listview_20220528.datas.StudentData
 
 class StudentAdapter(
@@ -9,4 +13,18 @@ class StudentAdapter(
     val resId: Int,
     val mList: ArrayList<StudentData>
 ) : ArrayAdapter<StudentData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if ( tempRow == null ) {
+            tempRow =  LayoutInflater.from( mContext ).inflate( R.layout.student_list_item, null )
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
 }
