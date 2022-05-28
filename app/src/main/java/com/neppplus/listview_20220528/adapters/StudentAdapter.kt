66ceb7919d891e03050c71aaddcf2afc468a4aae
조경진 <cho881020@gmail.com>
2022-05-28 11:42:37 +0900

    StudentAdpater(자식)이 추가 정보를 받아와서 => 부모에게 전달 => 상속 가능해짐. (에러 해결)

--- app/src/main/java/com/neppplus/listview_20220528/adapters/StudentAdapter.kt
+++ app/src/main/java/com/neppplus/listview_20220528/adapters/StudentAdapter.kt
@@ -1,7 +1,12 @@
 package com.neppplus.listview_20220528.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.neppplus.listview_20220528.datas.StudentData
 
-class StudentAdapter : ArrayAdapter<StudentData>() {
+class StudentAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StudentData>
+) : ArrayAdapter<StudentData>(mContext, resId, mList) {
 }
